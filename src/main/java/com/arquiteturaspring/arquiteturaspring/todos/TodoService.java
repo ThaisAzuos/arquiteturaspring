8e189634ae13ee4b099873693175e63be06e3ae2
ThaisAzuos <thais.azuoss@gmail.com>
2025-09-28 01:19:46 -0300

    Trabalhando com componentes

--- src/main/java/com/arquiteturaspring/arquiteturaspring/todos/TodoService.java
+++ src/main/java/com/arquiteturaspring/arquiteturaspring/todos/TodoService.java
@@ -6,17 +6,24 @@ public class TodoService {
 
 
     private TodoRepository repository;
+    private TodoValidator validator;
+    private MailSender mailSender;
 
-    public  TodoService(TodoRepository repository){
+    public TodoService(TodoRepository repository, TodoValidator validator, MailSender mailSender) {
         this.repository = repository;
+        this.validator = validator;
+        this.mailSender = mailSender;
     }
-
+    
     public TodoEntity salvar(TodoEntity novoTodo){
+        validator.validar(novoTodo);
         return repository.save(novoTodo);
     }
 
     public void atualizarStatus(TodoEntity todo){
         repository.save(todo);
+        String status = todo.getConcluido() == Boolean.TRUE ? "Concluído" : "Não concluído";
+        mailSender.enviar("Todo " + todo.getDescricao() + " foi atualizado para " + status);
     }
 
 
