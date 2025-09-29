2e9631632316d28008f478385f47a6cb9d688114
ThaisAzuos <thais.azuoss@gmail.com>
2025-09-29 01:02:35 -0300

    Escopo dos Beans

--- src/main/java/com/arquiteturaspring/arquiteturaspring/BeanGerenciado.java
+++ src/main/java/com/arquiteturaspring/arquiteturaspring/BeanGerenciado.java
@@ -3,9 +3,20 @@ package com.arquiteturaspring.arquiteturaspring;
 import com.arquiteturaspring.arquiteturaspring.todos.TodoEntity;
 import com.arquiteturaspring.arquiteturaspring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+//@Scope(WebApplicationContext.SCOPE_APPLICATION)
+//@Scope(WebApplicationContext.SCOPE_REQUEST)
+//@Scope(WebApplicationContext.SCOPE_SESSION)
+//@Scope("singleton")
+//@Scope("request")
+//@Scope("session")
+//@Scope("application")
 public class BeanGerenciado {
 
     //três formas de injetar dependência:
